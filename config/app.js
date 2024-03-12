e0ffab67072a024768f0e8e0b5aea2ce036f9c65
Denis Mamlev <81098489+DennyMaverick@users.noreply.github.com>
2024-03-12 18:52:32 +0300

    Add files via upload

---
+++ config/app.js
@@ -0,0 +1,29 @@
+const isProd = process.argv.includes("--production")
+const isDev = !isProd
+
+module.exports = {
+  isProd: isProd,
+  isDev: isDev,
+
+  htmlmin: {
+    collapseWhitespace: isProd,
+  },
+
+  webpack: {
+    mode: isProd ? "production" : "development",
+  },
+
+  imagemin: {
+    verbose: true,
+  },
+
+  fonter: {
+    formats: ["ttf", "woff", "eot", "svg"],
+  },
+
+  scss: {
+    importer: require("node-sass-tilde-importer"),
+    outputStyle: "compressed",
+  },
+
+}
